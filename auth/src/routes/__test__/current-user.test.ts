8e066322dfd70d4b5bf5cae7bb1e1ca6429bc964
stefan0982 <stefan.c.0608@mail.ru>
2022-01-18 17:59:14 +0200

    test should pass

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,7 +5,7 @@ it('should respond with details about the current user', async function () {
   const cookie = await global.signup()
 
   const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send({})
-    .expect(400)
+    .expect(200)
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
